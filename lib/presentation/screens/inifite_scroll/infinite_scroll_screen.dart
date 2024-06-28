ffe170c412d2ad7a8349cde20f35ef62f3cc7d76
jose7Rodriguez <jose7.balbuena@gmail.com>
2024-06-28 15:26:50 -0600

    Screens creadas y barril actualizado

---
+++ lib/presentation/screens/inifite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  const InfiniteScrollScreen({super.key});
+
+  static const name = 'animated_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiCOntrolsScreen extends StatelessWidget {
+  const UiCOntrolsScreen({super.key});
+
+  static const name = 'ui_controls_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
